435448a9b8817e884f8b15c6bae5745f9e057ea3
Deniz Aydinoglu <deniz.aydinoglu@gmail.com>
2024-03-02 17:25:22 +0300

    Add Somun connection and RPC call

--- example/somun_flutter_example.dart
+++ example/somun_flutter_example.dart
@@ -1,2 +1,20 @@
+import 'package:somun_flutter/somun_flutter.dart';
+
 void main() {
+
+  Somun.connect("localhost", 16666, () {
+    print("Connected");
+
+    Somun.call("Rpc_test", ["hello", 1]);
+
+    Future.delayed(Duration(seconds: 2), () {
+      Somun.disconnect();      
+    });
+
+  }, () {
+    print("Disconnected");
+  });
+
+
+
 }
