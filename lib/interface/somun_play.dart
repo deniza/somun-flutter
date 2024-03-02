3f202b121d1a315ea222de4c61683e7fc45eb5f7
Deniz Aydinoglu <deniz.aydinoglu@gmail.com>
2024-03-02 22:29:23 +0300

    Add game-related methods to SomunPlay class

--- lib/interface/somun_play.dart
+++ lib/interface/somun_play.dart
@@ -20,4 +20,62 @@ class SomunPlay extends SomunInterface {
 
   }
 
+  void resignGame(int gameId, {Function? responseHandler}) {
+
+    setResponseHandler("resignGameResponse", responseHandler);
+
+    call("resignGame", [gameId]);
+
+  }
+
+  void listGames({Function? responseHandler}) {
+
+    setResponseHandler("listGamesResponse", responseHandler);
+
+    call("listGames", []);
+
+  }
+
+  void makeMove(int gameId, String jsonData, {Function? responseHandler}) {
+
+    setResponseHandler("makeMoveResponse", responseHandler);
+
+    call("makeMove", [gameId, jsonData]);
+
+  }
+
+  void createRandomGame(int gameType, {Function? responseHandler}) {
+
+    setResponseHandler("createRandomGameResponse", responseHandler);
+
+    call("createRandomGame", [gameType]);
+
+  }
+
+  void createInvitation(int invitee, int gameType, bool shouldStartOnline) {
+
+    call("createInvitation", [invitee, gameType, shouldStartOnline]);
+
+  }
+
+  void listInvitations({Function? responseHandler}) {
+
+    setResponseHandler("invitationsList", responseHandler);
+
+    call("listInvitations", []);
+
+  }
+
+  void acceptInvitation(int invitationId) {
+
+    call("acceptInvitation", [invitationId]);
+
+  }
+
+  void rejectInvitation(int invitationId) {
+
+    call("rejectInvitation", [invitationId]);
+
+  }
+
 }
