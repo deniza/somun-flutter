d05a76e30307d716c22ad71e664c001b22a4395f
Deniz Aydinoglu <deniz.aydinoglu@gmail.com>
2024-03-05 16:35:07 +0300

    show game page

--- examples/example_game/lib/pages/lobby_page.dart
+++ examples/example_game/lib/pages/lobby_page.dart
@@ -1,7 +1,10 @@
+import 'package:example_game/pages/game_page.dart';
 import 'package:flutter/material.dart';
 
+// create a new stateless widget called LobbyPage that has a list that lists strings
+
 class LobbyPage extends StatelessWidget {
-  const LobbyPage({super.key});
+  const LobbyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +12,37 @@ class LobbyPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Lobby'),
       ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: const Text('Game 1'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const GamePage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Game 2'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const GamePage()),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Game 3'),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const GamePage()),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
-
 }
