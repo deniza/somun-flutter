30f53e6af88c0946a0e9334b99959c5912b01ce7
Deniz Aydinoglu <deniz.aydinoglu@gmail.com>
2024-03-06 17:27:22 +0300

    Add GameState class and update Game class

--- examples/example_game/lib/model/model.dart
+++ examples/example_game/lib/model/model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 GameList games = GameList();
@@ -5,9 +7,56 @@ GameList games = GameList();
 class Game {
 
   int gameId;
+  int turnOwnerId = -1;
   String gameName;
+  GameState? gameState;
 
   Game({required this.gameId, required this.gameName});
+
+  void setGameState(GameState gameState) {
+    this.gameState = gameState;
+  }
+
+}
+
+enum MoveStatus {
+  targetIsBelow,
+  targetIsAbove
+}
+
+class GameState {
+
+  final String _json;
+  
+  int winnerId = -1;
+  int numberToFind = -1;
+  MoveStatus lastMoveStatus = MoveStatus.targetIsBelow;
+  
+  GameState(this._json) {
+
+    jsonDecode(_json, reviver: (key, value) {
+      if (key == 'winner') {
+        winnerId = _toInt(value);
+      }
+      else if (key == 'target') {
+        lastMoveStatus = _toInt(value) == -1 ? MoveStatus.targetIsBelow : MoveStatus.targetIsAbove;
+      }
+      else if (key == 'numberToFind') {
+        numberToFind = _toInt(value);
+      }
+      return value;
+    });
+
+
+  }
+
+  int _toInt(numberObject) {    
+    return numberObject.toInt();
+  }
+
+  bool isFinished() {
+    return winnerId != -1;
+  }
 
 }
 
@@ -33,6 +82,10 @@ class GameList extends ChangeNotifier {
 
   Game getGame(int index) {
     return games[index];
+  }
+
+  Game getGameById(int gameId) {
+    return games.firstWhere((game) => game.gameId == gameId);
   }
 
   int get length {
